5a1040d27044648f9bf2c57d42f6a7b0f1074528
bhargava reddy iragam reddy <bhargava.iragam@ryancompanies.com>
2019-05-06 09:51:46 -0500

    Add project files.

---
+++ JDE_VendorOnboarding_SyncProcess/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JDE_VendorOnboarding_SyncProcess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
